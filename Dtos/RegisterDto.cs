97a56ce1b342d5abdefdb4968a7cd8fd47914475
izra4 <aryaizra2@gmail.com>
2024-11-15 17:31:06 +0700

    feat(api): user crud and fix register api)

---
+++ Dtos/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ticketing.Dtos
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
